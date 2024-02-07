5bf3f24884a4eec50cb1409af1d0d1b8b1b51c27
Clu-Codes <retsulcconnor@gmail.com>
2024-02-07 08:09:37 -0500

    added tests and fixed bug where wall status was not changing

--- maze.py
+++ maze.py
@@ -25,7 +25,7 @@ class Maze:
         self._win = win
 
         self._create_cells()
-        self._break_entrance_exit(self._cells[0][0], self._cells[-1][-1])
+        self._break_entrance_exit()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -53,19 +53,28 @@ class Maze:
         self._win.redraw()
         time.sleep(0.025)
 
-    def _break_entrance_exit(self, entrance_cell, exit_cell):
-        if self._win is None:
-            return
+    def _break_entrance_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
 
-        if entrance_cell.has_top_wall:
-            entrance_cell.has_top_wall = False
-            entrance_cell.draw(
-                entrance_cell._x1,
-                entrance_cell._y1,
-                entrance_cell._x2,
-                entrance_cell._y2,
-            )
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
 
-        if exit_cell.has_right_wall:
-            exit_cell.has_right_wall = False
-            exit_cell.draw(exit_cell._x1, exit_cell._y1, exit_cell._x2, exit_cell._y2)
+        # if self._win is None:
+        #     return
+
+        # entrance_cell = self._cells[0][0]
+        # exit_cell = self._cells[self._num_cols - 1][self._num_rows - 1]
+
+        # if entrance_cell.has_top_wall:
+        #     entrance_cell.has_top_wall = False
+        #     entrance_cell.draw(
+        #         entrance_cell._x1,
+        #         entrance_cell._y1,
+        #         entrance_cell._x2,
+        #         entrance_cell._y2,
+        #     )
+
+        # if exit_cell.has_bottom_wall:
+        #     exit_cell.has_bottom_wall = False
+        #     exit_cell.draw(exit_cell._x1, exit_cell._y1, exit_cell._x2, exit_cell._y2)
--- tests.py
+++ tests.py
@@ -18,7 +18,7 @@ class Test(unittest.TestCase):
         num_rows = 10
         mb = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(mb._cells[0][0].has_top_wall, False)
-        self.assertEqual(mb._cells[num_cols - 1][num_rows - 1].has_right_wall, False)
+        self.assertEqual(mb._cells[num_cols - 1][num_rows - 1].has_bottom_wall, False)
 
 
 if __name__ == "__main__":
