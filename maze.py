33d8310e7ec37e9b9c6a713f21166536a958bc1b
Clu-Codes <retsulcconnor@gmail.com>
2024-02-10 10:11:14 -0500

    maze created, unit tests added for wall breakdown of current and adjacent cells

--- maze.py
+++ maze.py
@@ -6,7 +6,16 @@ import time
 
 class Maze:
     def __init__(
-        self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win=None,
+        seed=None,
+        break_walls=True,
     ):
         self._cells = []
         self._x1 = x1
@@ -16,10 +25,12 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        self._seed = seed
-
+        if seed:
+            random.seed(seed)
         self._create_cells()
         self._break_entrance_exit()
+        if break_walls:
+            self._break_walls_r(0, 0)
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -45,7 +56,7 @@ class Maze:
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.025)
+        time.sleep(0.005)
 
     def _break_entrance_exit(self):
         self._cells[0][0].has_top_wall = False
@@ -55,75 +66,37 @@ class Maze:
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
 
     def _break_walls_r(self, i, j):
-        # Need to account for a cell being at the edge within the maze. That option must be eliminated
-        # Need to think through how to populate the not_visisted array
-        self._cells[i][j] = True
+        self._cells[i][j]._visited = True
         while True:
-            adj_cells = [
-                self._cells[i - 1][j],
-                self._cells[i + 1][j],
-                self._cells[1][j - 1],
-                self._cells[1][j + 1],
-            ]
             pos_dir = []
-            for cell in adj_cells:
-                if not cell._visited and cell in self._cells:
-                    pos_dir.append(cell)
+            if i > 0 and not self._cells[i - 1][j]._visited:
+                pos_dir.append((self._cells[i - 1][j], "left"))
+            if i < self._num_cols - 1 and not self._cells[i + 1][j]._visited:
+                pos_dir.append((self._cells[i + 1][j], "right"))
+            if j > 0 and not self._cells[i][j - 1]._visited:
+                pos_dir.append((self._cells[i][j - 1], "up"))
+            if j < self._num_rows - 1 and not self._cells[i][j + 1]._visited:
+                pos_dir.append((self._cells[i][j + 1], "down"))
 
             if len(pos_dir) == 0:
                 self._draw_cell(i, j)
                 return
             else:
-                rand_adj_cell = random.randrange(0, len(pos_dir) - 1)
-                top_line = Line(
-                    Point(self._cells[i][j]._x1, self._cells[i][j]._y1),
-                    Point(self._cells[i][j]._x2, self._cells[i][j]._y1),
-                )
-                left_line = Line(
-                    Point(self._cells[i][j]._x1, self._cells[i][j]._y1),
-                    Point(self._cells[i][j]._x1, self._cells[i][j]._y2),
-                )
-                right_line = Line(
-                    Point(self._cells[i][j]._x2, self._cells[i][j]._y1),
-                    Point(self._cells[i][j]._x2, self._cells[i][j]._y2),
-                )
-                bottom_line = Line(
-                    Point(self._cells[i][j]._x1, self._cells[i][j]._y2),
-                    Point(self._cells[i][j]._x2, self._cells[i][j]._y2),
-                )
+                rand_adj_cell, direction = random.choice(pos_dir)
 
-                adj_top_line = Line(
-                    Point(self.rand_adj_cell._x1, self.rand_adj_cell._y1),
-                    Point(self.rand_adj_cell._x2, self.rand_adj_cell._y1),
-                )
-                adj_left_line = Line(
-                    Point(self.rand_adj_cell._x1, self.rand_adj_cell._y1),
-                    Point(self.rand_adj_cell._x1, self.rand_adj_cell._y2),
-                )
-                adj_right_line = Line(
-                    Point(self.rand_adj_cell._x2, self.rand_adj_cell._y1),
-                    Point(self.rand_adj_cell._x2, self.rand_adj_cell._y2),
-                )
-                adj_bottom_line = Line(
-                    Point(self.rand_adj_cell._x1, self.rand_adj_cell._y2),
-                    Point(self.rand_adj_cell._x2, self.rand_adj_cell._y2),
-                )
-
-                if top_line == adj_bottom_line:
-                    self._cells[i][j].has_top_wall = False
-                    rand_adj_cell.has_bottom_wall = False
-                    self._break_walls_r(i - 1, j)
-                if left_line == adj_right_line:
+                if direction == "left":
                     self._cells[i][j].has_left_wall = False
                     rand_adj_cell.has_right_wall = False
-                    self._break_walls_r(i, j + 1)
-                if right_line == adj_left_line:
-                    self._cells[i][j].has_right_wall = False
-                    rand_adj_cell.has_left_wall = False
-                    self._break_walls_r(i, j - 1)
-                if bottom_line == adj_top_line:
+                    self._break_walls_r(i - 1, j)
+                if direction == "down":
                     self._cells[i][j].has_bottom_wall = False
                     rand_adj_cell.has_top_wall = False
+                    self._break_walls_r(i, j + 1)
+                if direction == "up":
+                    self._cells[i][j].has_top_wall = False
+                    rand_adj_cell.has_bottom_wall = False
+                    self._break_walls_r(i, j - 1)
+                if direction == "right":
+                    self._cells[i][j].has_right_wall = False
+                    rand_adj_cell.has_left_wall = False
                     self._break_walls_r(i + 1, j)
-
-                # Compare current cell with rand_adj_cell's x and y values to determine which walls need to be knocked down for each cell. Since each cell has their own wall, each cell's wall need to be flagged as false.
--- tests.py
+++ tests.py
@@ -6,7 +6,9 @@ class Test(unittest.TestCase):
     def test_maze_create_cells(self):
         num_cols = 12
         num_rows = 10
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m1 = Maze(
+            0, 0, num_rows, num_cols, 10, 10, win=None, seed=None, break_walls=False
+        )
         self.assertEqual(
             len(m1._cells),
             num_cols,
@@ -16,9 +18,58 @@ class Test(unittest.TestCase):
     def test_break_walls(self):
         num_cols = 12
         num_rows = 10
-        mb = Maze(0, 0, num_rows, num_cols, 10, 10)
+        mb = Maze(0, 0, num_rows, num_cols, 10, 10, win=None, seed=None)
         self.assertEqual(mb._cells[0][0].has_top_wall, False)
         self.assertEqual(mb._cells[num_cols - 1][num_rows - 1].has_bottom_wall, False)
+
+    def test_break_walls_r(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win=None, seed=0)
+
+        for i in range(num_cols):
+            for j in range(num_rows):
+                cell = m1._cells[i][j]
+                missing_walls = {
+                    direction: wall
+                    for (direction, wall) in [
+                        ("top", cell.has_top_wall),
+                        ("bottom", cell.has_bottom_wall),
+                        ("left", cell.has_left_wall),
+                        ("right", cell.has_right_wall),
+                    ]
+                    if not wall
+                }
+
+                key_list = list(missing_walls.keys())
+
+                if cell._visited:
+                    self.assertTrue(
+                        not cell.has_top_wall
+                        or not cell.has_bottom_wall
+                        or not cell.has_left_wall
+                        or not cell.has_right_wall
+                    )
+                    if key_list[0] == "left":
+                        self.assertTrue(
+                            not cell.has_left_wall,
+                            not m1._cells[i - 1][j].has_right_wall,
+                        )
+                    if key_list[0] == "right":
+                        self.assertTrue(
+                            not cell.has_right_wall,
+                            not m1._cells[i + 1][j].has_left_wall,
+                        )
+                    if key_list[0] == "up":
+                        self.assertTrue(
+                            not cell.has_top_wall,
+                            not m1._cells[i][j - 1].has_bottom_wall,
+                        )
+                    if key_list[0] == "down":
+                        self.assertTrue(
+                            not cell.has_bottom_wall,
+                            not m1._cells[i][j + 1].has_top_wall,
+                        )
 
 
 if __name__ == "__main__":
